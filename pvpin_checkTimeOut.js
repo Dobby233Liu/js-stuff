ed71888c0f914a1f42059e870df91a192ea5120e
Liu-Wen-Yuan <15816141883@163.com>
2018-02-25 21:03:53 +0800

    Create pvpin_checkTimeOut.js

---
+++ pvpin_checkTimeOut.js
@@ -0,0 +1,10 @@
+var pvpin_timeout=0;
+var pvpin_max_timeout;
+var pvpin_onTimeout;
+function pvpin_checkTimeout(){
+pvpin_timeout++;
+
+if(pvpin_max_timeout&&pvpin_timeout>=pvpin_max_timeout&&pvpin_onTimeout&&typeof pvpin_onTimeout==function){
+pvpin_onTimeout;
+}
+}
