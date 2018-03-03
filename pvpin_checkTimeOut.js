74ae892983a508ae5f88de03597c354facf83336
Liu-Wen-Yuan <15816141883@163.com>
2018-03-03 11:55:45 +0800

    Update pvpin_checkTimeOut.js

--- pvpin_checkTimeOut.js
+++ pvpin_checkTimeOut.js
@@ -4,7 +4,7 @@ var pvpin_onTimeout;
 function pvpin_checkTimeout(){
 pvpin_timeout++;
 
-if(pvpin_max_timeout&&pvpin_timeout>=pvpin_max_timeout&&pvpin_onTimeout&&typeof pvpin_onTimeout==function){
-pvpin_onTimeout;
+if(pvpin_max_timeout&&pvpin_timeout>=pvpin_max_timeout&&pvpin_onTimeout&&typeof pvpin_onTimeout=='function'){
+pvpin_onTimeout();
 }
 }
